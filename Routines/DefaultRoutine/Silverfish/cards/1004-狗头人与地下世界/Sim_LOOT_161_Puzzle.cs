87edd9d882d8fa328ba5e5b99c85032bab276d67
ztzz <ztzz.xyz@gmail.com>
2025-07-06 03:02:33 +0000

    Update to patch 33.0.0.223542
    (cherry picked from commit aa0c2c3f30ff56c6eeaffbd8d8a36a4872b0a328)

---
+++ Routines/DefaultRoutine/Silverfish/cards/1004-狗头人与地下世界/Sim_LOOT_161_Puzzle.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HREngine.Bots
+{
+	//随从 中立 费用：5 攻击力：4 生命值：6
+	//Carnivorous Cube
+	//食肉魔块
+	//<b>Battlecry:</b> Destroya friendly minion.<b>Deathrattle:</b> Summon 2 copies of it.
+	//<b>战吼：</b>消灭一个友方随从。<b>亡语：</b>召唤两个被消灭随从的复制。
+	class Sim_LOOT_161_Puzzle : SimTemplate
+	{
+		
+		
+	}
+}
